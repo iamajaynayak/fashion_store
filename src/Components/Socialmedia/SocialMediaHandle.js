eb1c06bf4a27742ce3b53af57cfc3b2a3867fd5e
Ajay Nayak <63513168+iamajaynayak@users.noreply.github.com>
2024-03-07 17:28:56 +0530

    Update SocialMediaHandle.js

--- src/Components/Socialmedia/SocialMediaHandle.js
+++ src/Components/Socialmedia/SocialMediaHandle.js
@@ -4,7 +4,7 @@ import "./socialmedia.css";
 const SocialMediaHandle = ({ profile, name, url }) => {
   return (
     <div className="social-media">
-      <a className="social-link" href={url} target="_blank">
+      <a className="social-link" href={url} target="_blank" rel="noopener noreferrer">
         <img src={profile} className="social-img" alt="social-media-logo" />
         <span className="social-name">{name}</span>
       </a>
